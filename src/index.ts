1db37a6331ecb3b13f955add6bf93dd8e5353e7e
vtuancv3 <v.vtuancv3@cloudhms.net>
2024-01-12 16:21:00 +0700

    update code

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express from 'express';
 
-import { APP_PATH } from './core/common/constants/index.constant.ts';
-import rootRouter from './routes/index.route.ts';
+// import { APP_PATH } from './core/common/constants/index.constant.ts';
+// import rootRouter from './routes/index.route.ts';
 
 const app = express();
 const port = 5000;
 
-app.use(APP_PATH, rootRouter);
+// app.use(APP_PATH, rootRouter);
+app.get('/', (req, res) => {
+    res.json('success');
+});
 
 app.listen(port, () => {
     console.log(`Listening on port http://localhost:${port}`);
